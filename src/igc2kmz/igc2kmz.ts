2b14d3e1fd49f7bd976190ea8e217d091e8d3d18
Sylvain Pasutto <sylvain.pasutto@prestataire.april.com>
2023-02-16 15:22:40 +0100

    ajout exemple html

--- src/igc2kmz/igc2kmz.ts
+++ src/igc2kmz/igc2kmz.ts
@@ -3,12 +3,15 @@
 //typescript? https://code.visualstudio.com/docs/languages/typescript
 //vs code task : https://code.visualstudio.com/docs/editor/tasks
 
-function sayHello(name: string): void {
-    console.log(`Hello ${name}!`);
-}
-  
-  sayHello('Dave');
+//const IGCParser = require('igc-parser');
+import IGCParser = require("igc-parser")
+import {IGCFile} from 'igc-parser'
 
+function sayHello(name: string, content: string): IGCFile {
+    console.log(`Hello ${name}!`);
+    let pouet = IGCParser.parse(content);
+    return pouet;
+}
 
 /*
 class IGC2KMZ {
