8bbbee9e43e69bcf9c1f8d9cb98a81d246917008
Sylvain PASUTTO <sylvainpasutto@gmail.com>
2023-03-22 21:52:06 +0100

    SPA bundle all js

--- build.mjs
+++ build.mjs
@@ -53,15 +53,20 @@ if (build) {
   }).catch(() => process.exit(1))
 } else {
   // BUNDLE
-  const regtoreplace = /<script[\s\r\n]+src=(?:"|')\.\/dist\/igc2kmz\.min\.js(?:"|')[\s\r\n]*>/i; //'<script src="./dist/igc2kmz.min.js">';
+  const regtoreplace = /<script[\s\r\n]+src\s*=\s*(?:"|')([^"']+)(?:"|')[\s\r\n]*>/i;
   let htmli2k = fs.readFileSync('./igc2kmz.html', { encoding: 'utf8', flag: 'r' });
-  const minifiedjs = fs.readFileSync('./dist/igc2kmz.min.js', { encoding: 'utf8', flag: 'r' });
+  let matches = null;
   if (!regtoreplace.test(htmli2k)) {
     throw new Error('Unable to find igc2kmz script tag in HTML!!!');
   } else {
-    //https://stackoverflow.com/a/34040529
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_the_replacement
-    htmli2k = htmli2k.replace(regtoreplace, () => ('<script>' + minifiedjs));
-    fs.writeFileSync('./dist/igc2kmz_spa.html', htmli2k);
+    let count = 0;
+    while ((matches = htmli2k.match(regtoreplace)) != null && count++ < 10) {
+      if (matches.length < 2) { console.log('pouet'); continue; }
+      const minifiedjs = fs.readFileSync(matches[1], { encoding: 'utf8', flag: 'r' });
+      //https://stackoverflow.com/a/34040529
+      //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_the_replacement
+      htmli2k = htmli2k.replace(regtoreplace, () => ('<script>' + minifiedjs));
+    }
   }
+  fs.writeFileSync('./dist/igc2kmz_spa.html', htmli2k);
 }
