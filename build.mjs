76de01209bb4aa18fb3a3246486baa4f1e36d513
Sylvain PASUTTO <sylvainpasutto@gmail.com>
2023-03-23 09:35:55 +0100

    bundle inline js/css

--- build.mjs
+++ build.mjs
@@ -50,19 +50,41 @@ await esbuild.build({
 
 if (bundle) {
   // BUNDLE
-  const regtoreplace = /<script[\s\r\n]+src\s*=\s*(?:"|')([^"']+)(?:"|')[\s\r\n]*>/i;
+  const reginsert = /<script[\s\r\n]+src\s*=\s*(?:"|')([^"']+)(?:"|')[\s\r\n]*>/i;
+  const regminifyjs = /<script>((?:.|[\r\n])*)<\/script>/i;
+  const regminifycss = /<style>((?:.|[\r\n])*)<\/style>/i;
   let htmli2k = fs.readFileSync('./igc2kmz.html', { encoding: 'utf8', flag: 'r' });
   let matches = null;
-  if (!regtoreplace.test(htmli2k)) {
+  let count = 0;
+  while ((matches = htmli2k.match(regminifyjs)) != null && count++ < 10) {
+    if (matches.length < 2) continue;
+    const minifiedjs = await esbuild.transform(matches[1], { loader: 'js', minify: true });
+    if (typeof minifiedjs.code === 'string') {
+      //https://stackoverflow.com/a/34040529
+      //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_the_replacement
+      htmli2k = htmli2k.replace(regminifyjs, () => ('<script>' + minifiedjs.code + '</script>'));
+    }
+  }
+  count = 0;
+  while ((matches = htmli2k.match(regminifycss)) != null && count++ < 10) {
+    if (matches.length < 2) continue;
+    const minifiedjs = await esbuild.transform(matches[1], { loader: 'css', minify: true });
+    if (typeof minifiedjs.code === 'string') {
+      //https://stackoverflow.com/a/34040529
+      //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_the_replacement
+      htmli2k = htmli2k.replace(regminifycss, () => ('<style>' + minifiedjs.code + '</style>'));
+    }
+  }
+  if (!reginsert.test(htmli2k)) {
     throw new Error('Unable to find igc2kmz script tag in HTML!!!');
   } else {
-    let count = 0;
-    while ((matches = htmli2k.match(regtoreplace)) != null && count++ < 10) {
+    count = 0;
+    while ((matches = htmli2k.match(reginsert)) != null && count++ < 10) {
       if (matches.length < 2) continue;
       const minifiedjs = fs.readFileSync(matches[1], { encoding: 'utf8', flag: 'r' });
       //https://stackoverflow.com/a/34040529
       //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_the_replacement
-      htmli2k = htmli2k.replace(regtoreplace, () => ('<script>' + minifiedjs));
+      htmli2k = htmli2k.replace(reginsert, () => ('<script>' + minifiedjs));
     }
   }
   fs.writeFileSync('./dist/igc2kmz_spa.html', htmli2k);
