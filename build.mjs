69850b08e591787cca78248d6be8b31ba2ab71e1
Sylvain PASUTTO <sylvainpasutto@gmail.com>
2023-03-24 16:14:00 +0100

    Release script

--- build.mjs
+++ build.mjs
@@ -1,6 +1,8 @@
 import * as esbuild from 'esbuild'
 //import inlineWorkerPlugin from 'esbuild-plugin-inline-worker';
 import * as fs from 'fs';
+import { simpleGit, CleanOptions } from 'simple-git';
+import JSZip from 'jszip';
 
 //const path = require('path')
 
@@ -16,12 +18,36 @@ var defaultconfig = {
   //watch: process.argv.includes("--watch"),
 };
 
+function buildRelease() {
+  simpleGit().tags().then(res => {
+    const regversion = /v(\d+\.\d+\.\d+)/i;
+    let matches = null;
+    if (!res || !(matches = res.latest.match(regversion)) || matches.length < 2) throw new Error('No valid version found');
+    console.log(`Releasing '${matches[1]}'...`);
+    let zipname = `igc2kmz-${matches[1]}.zip`;
+    let zip = new JSZip();
+    ['igc2kmz.cmd.js', 'igc2kmz.min.js', 'igc2kmz.js'].forEach(f => zip.file('dist/' + f, fs.readFileSync('dist/' + f, { encoding: 'utf8', flag: 'r' })));
+    zip.file('igc2kmz_spa.html', fs.readFileSync('dist/igc2kmz_spa.html', { encoding: 'utf8', flag: 'r' }));
+    ['igc2kmz.html', 'README.md', 'LICENSE'].forEach(f => zip.file(f, fs.readFileSync(f, { encoding: 'utf8', flag: 'r' })));
+    zip
+      .generateNodeStream({ type: 'nodebuffer', streamFiles: true })
+      .pipe(fs.createWriteStream(zipname))
+      .on('finish', function () {
+        console.log(zipname + " written.");
+      });
+  });
+}
+
 async function buildAction(buildmode) {
-  console.log(`Building '${buildmode}'...`);
-  let build = true, bundle = false;
+  let build = true, bundle = false, release = false;
   let config = {};
 
   switch (buildmode) {
+    case 'release':
+      release = true;
+      build = false;
+      bundle = false;
+      break;
     case 'bundle':
       build = false;
       bundle = true;
@@ -55,6 +81,7 @@ async function buildAction(buildmode) {
   }
 
   if (build) {
+    console.log(`Building '${buildmode}'...`);
     await esbuild.build(config).catch(() => process.exit(1));
     // contournement d'un bug dans collections.js utilisée par igc-xc-score ;
     // dans le fichier generic-collections.js est référencé directement l'objet global
@@ -69,6 +96,7 @@ async function buildAction(buildmode) {
     }
   }
   if (bundle) {
+    console.log(`Bundling igc2html_spa.html...`);
     // BUNDLE
     const reginsert = /<script[\s\r\n]+src\s*=\s*(?:"|')([^"']+)(?:"|')[\s\r\n]*>/i;
     const regminifyjs = /<script>((?:.|[\r\n])*)<\/script>/i;
@@ -109,8 +137,16 @@ async function buildAction(buildmode) {
     }
     fs.writeFileSync('./dist/igc2kmz_spa.html', htmli2k);
   }
+  if (release) {
+    buildRelease();
+  }
 }
 
-let argv = process.argv.slice(2);
-if (argv.length <= 0) argv.push('cmd', 'web', 'minify', 'bundle');
+let allargs = ['cmd', 'web', 'minify', 'bundle'];
+let argv = process.argv.slice(2).map(v => v.trim().toLowerCase());
+if (argv.length <= 0) {
+  argv = allargs;
+} else if (argv.indexOf('release') > -1) {
+  argv = [...allargs, 'release'];
+}
 argv.forEach(await buildAction);
