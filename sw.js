490151f9cacca5c27dfb6118973d16a4736f7d19
Sylvain PASUTTO <sylvainpasutto@gmail.com>
2023-04-01 09:35:16 +0200

    Version : 1.0.3.8

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //self.importScripts('data/games.js');
 
 // Files to cache
-const currentVersion = 'v1.0.3.7';
+const currentVersion = 'v1.0.3.8';
 const cacheName = 'igc2kmz-' + currentVersion;
 const appShellFiles = [
   '/igc2kmz/',
